d357f81096248888f0fbf020ee7ebedce88e02e0
Rangga <ranggaaldosas@gmail.com>
2023-12-09 12:31:34 +0700

    feat:update json

--- screens/details/SelectRoom.jsx
+++ screens/details/SelectRoom.jsx
@@ -5,44 +5,45 @@ import { COLORS, SIZES } from "../../constants/theme";
 import ReusableTile from "../../components/Reusable/ReusableTile";
 
 const SelectRoom = ({navigation}) => {
+
+  
   const rooms = [
     {
       _id: "64c631650298a05640539adc",
       country_id: "64c62bfc65af9f8c969a8d04",
-      title: "Walt Disney World",
-      location: "U.S.A New York ",
+      title: "Kos Putra Pak Karyo",
+      location: "Keputih, Surabaya ",
       imageUrl:
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/731e1f89-c028-43ef-97ee-8beabde696b6-vinci_01_disney.jpg",
+        "https://www.99.co/id/panduan/wp-content/uploads/2022/11/peraturan-kos-kosan-1000x630.jpg",
       rating: 4.7,
       review: "1204 Reviews",
     },
     {
       _id: "64d062a3de20d7c932f1f70a",
       country_id: "64c62bfc65af9f8c969a8d04",
-      title: "Statue of Liberty",
-      location: "U.S.A New York ",
-      imageUrl:
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/c3a8b882-b176-47f0-aec5-a0a49bf42fcd-statue-of-liberty-1.webp",
+      title: "AB Kost",
+      location : "Keputih, Surabaya",
+      imageUrl : "https://www.99.co/id/panduan/wp-content/uploads/2022/11/peraturan-kos-kosan-1000x630.jpg",
       rating: 4.8,
       review: "1452 Reviews",
     },
     {
       _id: "64d09e3f364e1c37c8b4b13c",
       country_id: "64c62bfc65af9f8c969a8d04",
-      title: "Golden Gate Bridge",
-      location: "U.S.A New York ",
+      title: "Kos Pak Dzaki (Putra)",
+      location: "Depok, Yogyakarta",
       imageUrl:
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/7b7b76aa-bbe0-4ca4-b52f-e2b82afa3a77-Golden-Gate-Bridge-San-Francisco.webp",
+        "https://www.99.co/id/panduan/wp-content/uploads/2022/11/peraturan-kos-kosan-1000x630.jpg",
       rating: 4.6,
       review: "2145 Reviews",
     },
     {
       _id: "64d09f90364e1c37c8b4b140",
       country_id: "64c62bfc65af9f8c969a8d04",
-      title: "Yellowstone National Park",
-      location: "U.S.A New York ",
+      title: "Kos Merpati (Putri)",
+      location: "Keputih, Surabaya",
       imageUrl:
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/f3f44363-f250-4002-88a8-19fe79169cc7-geyser-yelowstone-burst_h.webp",
+        "https://www.99.co/id/panduan/wp-content/uploads/2022/11/peraturan-kos-kosan-1000x630.jpg",
       rating: 4.8,
       review: "24455 Reviews",
     },
@@ -50,8 +51,8 @@ const SelectRoom = ({navigation}) => {
       _id: "64d30f789d008909fa8b7ce5",
       country_id: "64d2fd32618522e2fb342eec",
       imageUrl:
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/f3f44363-f250-4002-88a8-19fe79169cc7-geyser-yelowstone-burst_h.webp",
-      title: "Yellowstone National Park",
+        "https://www.99.co/id/panduan/wp-content/uploads/2022/11/peraturan-kos-kosan-1000x630.jpg",
+      title: "Kos Putra Pak Asril",
       location: "U.S.A New York ",
       rating: 4.8,
       review: "24455 Reviews",
